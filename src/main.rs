dc7ede9493cd7004e9b3cce5c061d0e6b8920952
shiva-patt-work <shiva.patt.work@gmail.com>
2023-09-10 03:20:50 -0500

    feat: :tada: Initialize

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use dioxus::prelude::*;
+use dioxus_fullstack::prelude::*;
+
+fn main() {
+    LaunchBuilder::new(app).launch();
+}
+
+fn app(cx: Scope) -> Element {
+    let mut count = use_state(cx, || 0);
+
+    cx.render(rsx! {
+        h1 { "High-Five counter: {count}" }
+        button { onclick: move |_| count += 1, "Up high!" }
+        button { onclick: move |_| count -= 1, "Down low!" }
+    })
+}
